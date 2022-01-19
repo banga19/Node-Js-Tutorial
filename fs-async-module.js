4350de6ed6da1defb67312ffe9a482110291eb4e
banga19 <bangalifof16@gmail.com>
2022-01-19 15:55:19 +0300

    second commit

--- fs-async-module.js
+++ fs-async-module.js
@@ -1,32 +1,15 @@
 
 // !! >>fs-module async method where we use callback functions 
-const {readFile, writeFile} = require('fs')
+const {readFile} = require('fs')
 
-console.log('start');
-readFile('./content/subfolder/first.txt', 'utf8', (err, result) => {
-    if(err) {
-        console.log(err)
+console.log('started a first task')
+// CHECK FILE PATH
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err);
         return
     }
-    const first = result
-    readFile(
-        './content/subfolder/second.txt', 'utf8', (err, result) => {
-            if(err) {
-                console.log(err);
-                return
-            }
-            const Second = result
-            writeFile(
-                './content/subfolder/result-async',
-                `here is the result: ${first}, ${Second}`,
-                (err,result) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    console.log('Done with this task');
-                }
-            )
-        }
-    )
+    console.log(result);
+    console.log("completed first task");
 })
-console.log(' start the next task');
+console.log("starting next task");
